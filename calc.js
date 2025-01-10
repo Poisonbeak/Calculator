a696a016b733532a4dd9edd78fcee6ac30a2d315
Poisonbeak <michele.alfano@davincimilazzo.edu.it>
2025-01-10 12:52:35 +0100

    calc with normal functions

--- calc.js
+++ calc.js
@@ -1,1 +1,42 @@
-let a = 1;
+const prompt = require("prompt-sync")();
+
+console.log(`
++ Add
+- Sub
+* Mul
+/ Div
+`)
+let operation = prompt();
+let op1 = Number(prompt(`first operand: `));
+let op2 = Number(prompt(`second operand: `));
+let result = 0;
+switch (operation) {
+    case '+':
+        result = sum(op1, op2);
+        break;
+    case '-':
+        result = sub(op1, op2);
+        break;
+    case '*':
+        result = mul(op1, op2);
+        break;
+    case '/':
+        result = div(op1, op2);
+        break;
+    default:
+        break;
+}
+console.log(`The result is: ${result}`);
+
+function sum(a, b) {
+    return a + b;      
+}
+function sub(a, b) {
+    return a - b;      
+}
+function mul(a, b) {
+    return a * b;      
+}
+function div(a, b) {
+    return a / b;      
+}
